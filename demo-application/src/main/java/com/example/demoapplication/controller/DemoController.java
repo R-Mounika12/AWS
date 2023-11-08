aee23682a51b7bd8751fbc61e6628d6cb471cc68
R-Mounika12 <91323516+R-Mounika12@users.noreply.github.com>
2023-11-08 20:37:30 +0530

    Update DemoController.java

--- demo-application/src/main/java/com/example/demoapplication/controller/DemoController.java
+++ demo-application/src/main/java/com/example/demoapplication/controller/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @RequestMapping("/hello")
     public String greeting() {
-        return "Welcome to the world of JAVA";
+        return "Welcome to the world of JAVA!!!!";
     }
 }
